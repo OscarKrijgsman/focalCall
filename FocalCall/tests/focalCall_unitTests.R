b5f741bb4473fd0954ab7493e40c836739830c65
Oscar Krijgsman <oscarkrijgsman@gmail.com>
2014-10-06 14:59:43 +0200

    Added more unitTests Removed "export all" calls NAMESPACE

--- FocalCall/tests/focalCall_unitTests.R
+++ FocalCall/tests/focalCall_unitTests.R
@@ -5,3 +5,10 @@ data(BierkensCNA)
 print(CGHset)
 stopifnot(inherits(CGHset, "cghCall"))
 
+print(CNVset)
+stopifnot(inherits(CNVset, "data.frame"))
+
+ExampleRun<-focalCall(CGHset, CNVset, focalSize=3, minFreq=2)
+print(ExampleRun)
+stopifnot(inherits(ExampleRun, "cghCall"))
+
